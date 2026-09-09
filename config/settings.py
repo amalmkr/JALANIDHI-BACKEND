ac7823d8fbbc2b57846286713bf260774d84227e
amalmkr <amalmkr01@gmail.com>
2026-09-09 20:17:17 +0530

    configure databases for railway

--- config/settings.py
+++ config/settings.py
@@ -109,11 +109,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jalanidhi',
-        'USER': 'root',
+        'NAME': os.getenv('DB_NAME', 'jalanidhi'),
+        'USER': os.getenv('DB_USER', 'root'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST','localhost'),
-        'PORT': os.getenv('DB_PORT','3306')
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
 
