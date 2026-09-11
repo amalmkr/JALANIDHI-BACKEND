b9c205a540bc66efcbfa10a949d57ad57ff5b24e
amalmkr <amalmkr01@gmail.com>
2026-09-11 17:08:24 +0530

    fixed serve media files in production

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +11,11 @@ urlpatterns = [
     path('api/',include("meter_readings.urls")),
     path('api/',include("complaints.urls")),
     path('api/',include("billing.urls")),
+
+    path(
+    "media/<path:path>",
+    serve,
+    {"document_root":settings.MEDIA_ROOT},
+),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
